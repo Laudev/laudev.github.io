dbb3a279f3f9af3ef848b04e81d5b536448b2631
Laudev <39934805+Laudev@users.noreply.github.com>
2020-02-07 04:31:08 +0100

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,23 +1,22 @@
-﻿var Typer = {
+var Typer = {
     text: null,
+    accessCountimer: null,
 
     index: 0,
+    speed: 2,
+    file: "",
     accessCount: 0,
     deniedCount: 0,
-    speed: 30,
-
-    file: "",
 
     init: function () {
-        setInterval(function () {
-            Typer.updLstChr();
+        accessCountimer = setInterval(function() {
+             Typer.updLstChr();
         }, 500);
 
         $.get(Typer.file, function (data) {
-                Typer.text = data;
-                Typer.text = Typer.text.slice(0, Typer.text.length - 1);
-            }
-        );
+            Typer.text = data;
+            Typer.text = Typer.text.slice(0, Typer.text.length - 1);
+        });
     },
 
     content: function () {
@@ -121,10 +120,3 @@ function t() {
         clearInterval(timer);
     }
 }
-
-//Temp function to log my keycodes
-//$(function () {
-//    $(document).keyup(function (e) {
-//        console.log(e.keyCode);
-//    });
-//});
